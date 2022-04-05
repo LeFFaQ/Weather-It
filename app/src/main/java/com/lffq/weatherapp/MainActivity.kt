a14c78aeefdab78381a9d01004717fb8e05c999d
Danil <lffqyx@yandex.ru>
2022-04-05 20:57:04 +0700

    #0001 Коммит. Удаляем лишнее и добавляем комментарии

--- app/src/main/java/com/lffq/weatherapp/MainActivity.kt
+++ app/src/main/java/com/lffq/weatherapp/MainActivity.kt
@@ -13,31 +13,22 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.lffq.weatherapp.ui.theme.WeatherTutorialTheme
 
 class MainActivity : ComponentActivity() {
+
+    // Точка входа входа в приложение
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Эта функция будет рисовать наш интерфейс
         setContent {
+
+            // Тема Нашего приложения
+            // Находится в файле Theme.kt нашего приложения
+            // Это Composable-функция, в которой
+            // Указываются цвета светлой/темной тем,
+            // Цвет заднего фона и определенный компонентов.
             WeatherTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WeatherTutorialTheme {
-        Greeting("Android")
     }
 }
