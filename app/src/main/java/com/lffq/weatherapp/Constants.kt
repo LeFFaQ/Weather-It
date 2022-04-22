6058c6f5532a610c8649e99a5dcb3a078def9124
Danil <lffqyx@yandex.ru>
2022-04-22 23:04:58 +0700

    #0011.1 Коммит. Constants.kt

---
+++ app/src/main/java/com/lffq/weatherapp/Constants.kt
@@ -0,0 +1,64 @@
+package com.lffq.weatherapp
+
+
+val _dayIcons = mapOf(
+    "01d" to R.raw.clear_day,
+    "02d" to R.raw.overcast_day,
+    "03d" to R.raw.overcast_day,
+    "04d" to R.raw.extreme_day,
+    "09d" to R.raw.overcast_day_drizzle,
+    "10d" to R.raw.overcast_day_rain,
+    "11d" to R.raw.thunderstorms_day_overcast,
+    "13d" to R.raw.overcast_day_snow,
+    "50d" to R.raw.overcast_day_haze,
+)
+
+val _nightIcons = mapOf(
+    "01n" to R.raw.clear_night,
+    "02n" to R.raw.overcast_night,
+    "03n" to R.raw.overcast_night,
+    "04n" to R.raw.extreme_night,
+    "09n" to R.raw.overcast_night_drizzle,
+    "10n" to R.raw.overcast_night_rain,
+    "11n" to R.raw.thunderstorms_night_overcast,
+    "13n" to R.raw.overcast_night_snow,
+    "50n" to R.raw.overcast_night_haze,
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
