a9c437b54ad08f490ad67e44b268a335618a0a08
Danil <lffqyx@yandex.ru>
2022-04-22 23:05:20 +0700

    #0011.1 Коммит. Constants.kt

--- app/src/main/java/com/lffq/weatherapp/network/models/current/Main.kt
+++ app/src/main/java/com/lffq/weatherapp/network/models/current/Main.kt
@@ -4,21 +4,23 @@ import com.google.gson.annotations.SerializedName
 
 data class Main(
 
-	@field:SerializedName("temp")
-	val temp: Number,
+    @field:SerializedName("temp")
+    val temp: Number,
 
-	@field:SerializedName("temp_min")
-	val tempMin: Number,
+    @field:SerializedName("temp_min")
+    val tempMin: Number,
 
-	@field:SerializedName("humidity")
-	val humidity: Number,
+    @field:SerializedName("humidity")
+    val humidity: Number,
 
-	@field:SerializedName("pressure")
-	val pressure: Number,
+    @field:SerializedName("pressure")
+    val pressure: Number,
 
-	@field:SerializedName("feels_like")
-	val feelsLike: Number,
+    @field:SerializedName("feels_like")
+    val feelsLike: Number,
 
-	@field:SerializedName("temp_max")
-	val tempMax: Number
+    @field:SerializedName("temp_max")
+    val tempMax: Number
+
+
 )
